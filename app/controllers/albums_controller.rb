929af8440f515510d369805d45fa5001abf936a2
Nurullo <khuwaidullaev@gmail.com>
2020-05-20 17:58:05 +0500

    RubyOR

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,10 @@
+class AlbumsController < ApplicationController
+    def index
+        @albums = Album.all
+    end
+
+    def show
+        @album = Albums.find(params[:id])
+        @tracks = @album.tracks
+    end
+end
